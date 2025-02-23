aa5100d02ddb914bfa70ecbe6d874284d5693862
이효규 <lhg961006@gmail.com>
2025-02-23 14:07:42 +0900

    Initial commit: Spring Boot project

---
+++ src/main/java/com/lhg1006/secondhandmarketplaceboot/SecondhandMarketplaceBootApplication.java
@@ -0,0 +1,13 @@
+package com.lhg1006.secondhandmarketplaceboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondhandMarketplaceBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondhandMarketplaceBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lhg1006/secondhandmarketplaceboot/SecondhandMarketplaceBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhg1006.secondhandmarketplaceboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondhandMarketplaceBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
